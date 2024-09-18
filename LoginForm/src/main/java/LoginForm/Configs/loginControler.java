db7364d39ca512a5a6ad05b8d76fe3446b693308
an <an@LAPTOPSIEUBEN>
2024-09-19 00:24:25 +0700

    create a maven project, configure three tier architecture, testing loginControler.java

---
+++ LoginForm/src/main/java/LoginForm/Configs/loginControler.java
@@ -0,0 +1,49 @@
+package LoginForm.Configs;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class loginControler
+ */
+
+
+
+@WebServlet(urlPatterns = {"/login"})
+public class loginControler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginControler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rd = request.getRequestDispatcher("view/Login.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		pw.println(request.getParameter("username"));
+		pw.println(request.getParameter("password"));
+	}
+
+}
